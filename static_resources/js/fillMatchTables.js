55067437013a060f41248401418a2a694f91c695
Rasmus Olsson <r.olsson96@gmail.com>
2020-03-12 02:23:59 +0100

    content data is filled with time, hard coded odds

--- static_resources/js/fillMatchTables.js
+++ static_resources/js/fillMatchTables.js
@@ -4,11 +4,39 @@ function fillMatchTables(jsonData) {
     console.log(jsonData);
 
     for (var i = 0; i < jsonData.matches.length; i++) {
+
+        var match = jsonData.matches[i];
+
         var matchRow = document.createElement("div");
 
         matchRow.className = "content-row";
 
-        matchRow.innerHTML = jsonData.matches[i].team1.name + " - " + jsonData.matches[i].team2.name;
+        var teams = document.createElement("div");
+        teams.innerHTML = match.team1.name + " - " + match.team2.name;
+        teams.className = "content-row-item teams";
+
+
+        var odds1 = document.createElement("div");
+        odds1.innerHTML = "1.00";
+        odds1.className = "content-row-item odds1";
+        var odds2 = document.createElement("div");
+        odds2.innerHTML = "2.00";
+        odds2.className = "content-row-item odds2";
+        var time = document.createElement("div");
+        time.className = "content-row-item matchTime";
+
+        if (match.live) {
+            time.innerHTML = "live";
+        } else {
+            var date = new Date(match.date);
+            var timeStr = date.toUTCString();
+            time.innerHTML = timeStr;
+        }
+
+        matchRow.appendChild(teams);
+        matchRow.appendChild(odds1);
+        matchRow.appendChild(odds2);
+        matchRow.appendChild(time);
 
         tabContent.appendChild(matchRow);
     }
