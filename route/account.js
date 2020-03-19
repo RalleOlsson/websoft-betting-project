592bb16b186070ef1205b81487f1e411e650c659
Rasmus Olsson <r.olsson96@gmail.com>
2020-03-19 19:05:39 +0100

    api\account\;C:\Program Files\Git\userId changes

--- route/account.js
+++ route/account.js
@@ -13,21 +13,39 @@ router.get('/', checkAuthenticated, (req, res) => {
 });
 
 router.get('/:userId(*)', checkAuthenticated, (req, res) => {
+
     fetch('http://localhost:1337/api/bets/user/' + req.user.userId)
         .then((response) => {
             return response.json();
         }).then((jsonData) => {
+            var matchData = {};
+            for (var i = 0; i < jsonData.length; i++) {
+                var bet = jsonData[i];
+                var url1 = 'http://localhost:1337/api/matches/' + bet.match_matchId + "/" + bet.game;
+                var url2 = 'http://localhost:1337/api/odds/' + bet.match_matchId;
+                Promise.all([fetch(url1), fetch(url2)]).then(function(results) {
+                    matchData.team1name = results[0].team1[0].name;
+                    matchData.team2name = results[0].team2[0].name;
+                    matchData.team1Odds = results[1].team1odds;
+                    matchData.team2Odds = results[1].team2odds;
+                });
+            }
             res.render("account", {
                 email: req.user.email,
                 userId: req.user.userId,
                 isAdmin: req.user.isAdmin,
                 balance: req.user.balance,
-                data: jsonData
+                betData: jsonData,
+                matchData: matchData
             });
         });
 
 
 });
+
+function fetchMatchData(jsonData) {
+
+}
 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
